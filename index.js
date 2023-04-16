6cef9c070f5e2ab24630fb2d0b4060913b09dc16
israel diaz <israel.diaz1701@gmail.com>
2023-04-15 19:00:50 -0700

    Final Control Flow

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+let feet = 2600;
+function scuberGreetingForFeet(feet){
+  if (feet <= 400){
+    let greeting = 'This one is on me!';
+    return greeting;
+  } else if (feet > 400 && feet < 2000){
+    let greeting = 'That will be twenty bucks.';
+    return greeting;
+  } else if (feet > 2000 && feet < 2500){
+    let greeting = 'I will gladly take your thirty bucks.';
+    return greeting;
+  } else if (feet >= 2500){
+    let greeting = 'No can do.';
+    return greeting;
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return city === "NYC" ? "Ok, sounds good." : "No go.";
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch(tip){
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return'Thank you.';
+    default:
+      return 'Bye.';
+  }
 }
+
+greeting;
+
+
